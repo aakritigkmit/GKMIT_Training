27bb1b363cb0eae7f5f260fb3b255c05609afcfa
aakritigkmit <aakriti@gkmit.co>
2024-09-25 23:38:08 +0530

    feat : sql queries changes in homework

--- SQL/Aggregate.sql
+++ SQL/Aggregate.sql
@@ -13,14 +13,16 @@ select
 	sum("UnitPrice"),
 	max("Milliseconds") ,
 	min("Bytes")
-	from 
+from 
   	"Track";
 
 
 -- Ques2. Calculate kilobyte / second using bytes and milliseconds from the “Track” table.
 select
     "TrackId",
-    ("Bytes"/ 1024) / ("Milliseconds" / 1000) AS "KiloBytesPerSec"
+    ("Bytes"/ 1024) / ("Milliseconds" / 1000) 
+ AS 
+ "KiloBytesPerSec"
 from
     "TrackId";
 
@@ -29,18 +31,25 @@ from
 -- Ques4 Calculate the count of people by their ‘city’ and sort them alphabetically from the “Employee” table.
 select "City",
 count("FirstName")
-from "Employee"
-group by "City","FirstName"
-order by "FirstName";
+from 
+"Employee"
+group by 
+"City","FirstName"
+order by 
+"FirstName";
 
 
 
 SELECT "City", 
-COUNT(*) 
-AS employee_count
-FROM "Employee"
-GROUP BY "City","FirstName"
-ORDER BY "City";
+	COUNT(*) 
+	AS 
+	employee_count
+FROM 
+"Employee"
+GROUP BY 
+"City","FirstName"
+ORDER BY 
+"City";
 
 
 
@@ -52,12 +61,15 @@ ORDER BY "City";
 
 
 select 
-count("InvoiceId"),
-sum("Total")
-from "Invoice"
-where "InvoiceDate">='2009-01-01' and "InvoiceDate"<='2009-03-31';
+	count("InvoiceId"),
+	sum("Total")
+from 
+"Invoice"
+where 
+"InvoiceDate">='2009-01-01' and "InvoiceDate"<='2009-03-31';
 
-
+-- Explanation:- used aggregate Functions to fetch sum and count 
+-- and then selected the range o
 
 
 
